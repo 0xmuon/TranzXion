81ae4ff8f4f37bca1f45bc69a161028f02c2fd78
Rudraksh Joshi <u22ee054@eed.svnit.ac.in>
2024-11-08 22:19:50 +0530

    added ./.github repo

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -5,12 +5,22 @@ export const GET = async () => {
     rules: [
       // map all root level routes to an action
       {
+<<<<<<< HEAD
         pathPattern: "/",
         apiPath: "/api/donate/",
       },
       {
         pathPattern: "/api/actions/",
         apiPath: "/api/actions/",
+=======
+        pathPattern: "/*",
+        apiPath: "/api/actions/*",
+      },
+      // idempotent rule as the fallback
+      {
+        pathPattern: "/api/actions/**",
+        apiPath: "/api/donate",
+>>>>>>> b6f7f99 (added ./.github repo)
       },
     ],
   };
@@ -22,4 +32,8 @@ export const GET = async () => {
 
 // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
 // THIS WILL ENSURE CORS WORKS FOR BLINKS
+<<<<<<< HEAD
 export const OPTIONS = GET;
+=======
+export const OPTIONS = GET;
+>>>>>>> b6f7f99 (added ./.github repo)
